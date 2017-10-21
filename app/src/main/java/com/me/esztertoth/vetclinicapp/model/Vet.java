a21e34176a745ce193fe4c839759c84328207f11
Toth Eszter Judit <t.eszterjudit@gmail.com>
2017-10-21 23:43:15 +0200

    remove first name and last name from vet

--- app/src/main/java/com/me/esztertoth/vetclinicapp/model/Vet.java
+++ app/src/main/java/com/me/esztertoth/vetclinicapp/model/Vet.java
@@ -11,10 +11,6 @@ public class Vet extends User implements Serializable {
     @SerializedName("id")
     @Expose
     private Long id;
-    @SerializedName("firstName")
-    private String firstName;
-    @SerializedName("lastName")
-    private String lastName;
     @SerializedName("speciality")
     private List<PetType> speciality;
 
@@ -24,22 +20,6 @@ public class Vet extends User implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public List<PetType> getSpeciality() {
